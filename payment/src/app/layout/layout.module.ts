950f453e4a64b6b44bf3eecf020aaa2becb6cbbd
Rezi <138244178+rezii3@users.noreply.github.com>
2024-10-19 15:42:57 +0400

    letstart

---
+++ payment/src/app/layout/layout.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[FooterComponent,NavBarComponent]
+})
+export class LayoutModule { }
